90cfe0e59c213d0acbf35d27e62e7a47a911bc8e
Jatin Tilwani <jatintilwani500@gmail.com>
2024-03-19 00:11:33 +0530

    adding undo redo functionality

--- Dendrite.ai-fabric/src/components/WhiteBoard.jsx
+++ Dendrite.ai-fabric/src/components/WhiteBoard.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState, useRef } from "react";
 import { FabricJSCanvas, useFabricJSEditor } from "fabricjs-react";
 import { useBoard } from "../contexts/Board";
 import ToolsList from "./ToolsList";
@@ -6,12 +6,42 @@ import ToolsList from "./ToolsList";
 const WhiteBoard = () => {
   const { items, tool, setItems, setTool, editor, onReady } = useBoard();
 
-  const handleKeyDown = (e) => {
-    if (e.key === "f" && editor) {
-      console.log("pressed")
-      const test = editor.zoomIn()
+  const history = []
+  let isWaiting = useRef(false);
+
+  const undo = useCallback(() => {
+    if (editor.canvas._objects.length > 0) {
+      history.push(editor.canvas._objects.pop())
     }
-    if (e.key === "Delete" && editor) {
+    editor.canvas.renderAll()
+  }, [editor])
+
+  const redo = useCallback(() => {
+    if (history.length > 0) {
+      editor.canvas.add(history.pop())
+    }
+  }, [editor])
+
+  
+  // Handling Keydown functions below, undo, redo and other stuff
+
+  const handleKeyDown = useCallback((e) => {
+
+    if (e.key === "z" && e.ctrlKey && !isWaiting.current) {
+      isWaiting.current = true;
+      setTimeout(() => {
+        isWaiting.current = false;
+      }, 200)
+      undo()
+    }
+    else if (e.key === "y" && e.ctrlKey && !isWaiting.current) {
+      isWaiting.current = true;
+      setTimeout(() => {
+        isWaiting.current = false;
+      }, 200)
+      redo();
+    }
+    else if (e.key === "Delete" && editor) {
       const activeObject = editor.canvas.getActiveObject();
       if (activeObject) {
         editor?.deleteSelected()
@@ -19,9 +49,15 @@ const WhiteBoard = () => {
         setItems(editor.canvas.toJSON());
       }
     }
-  };
+  }, [undo, redo, editor, setItems]);
 
-  document.addEventListener("keydown", handleKeyDown);
+
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => document.removeEventListener("keydown", handleKeyDown);
+  }, [handleKeyDown]);
 
   useEffect(() => {
     if (tool === "pencil") {
@@ -52,15 +88,20 @@ const WhiteBoard = () => {
   };
 
   return (
-    <div className="w-full h-full my-7 flex justify-center items-center">
-      <ToolsList />
-      <div className="flex justify-center items-center rounded-2xl w-[80vw] h-[90vh] overflow-hidden border-2">
-        <FabricJSCanvas
-          className="sample-canvas h-full border-green-800 w-full cursor-crosshair bg-gray-50"
-          onReady={onReady}
-        />
+    <>
+
+      <div className="w-full h-full my-7 flex justify-center items-center">
+        <ToolsList />
+        <div className="flex justify-center items-center rounded-2xl w-[80vw] h-[90vh] overflow-hidden border-2">
+          <FabricJSCanvas
+            className="sample-canvas h-full border-green-800 w-full cursor-crosshair bg-gray-50"
+            onReady={onReady}
+          />
+        </div>
       </div>
-    </div>
+      <button onClick={undo}>Undo</button>
+      <button onClick={redo}>Redo</button>
+    </>
   );
 };
 
